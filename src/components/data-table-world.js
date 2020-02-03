6903c9f9c45f9710bac24e271fbc374662570ccc
Tan Sy <sytanta@yahoo.com>
2020-02-03 23:36:07 +0700

    Add updated date

--- src/components/data-table-world.js
+++ src/components/data-table-world.js
@@ -1,43 +1,51 @@
 import React from "react"
 
 const DataTableWorld = ({ data }) => {
-  const { total, cured, death } = data.reduce((acc, item) => {
-    return {
-      total: acc.total + +item.node.infected,
-      cured: acc.cured + +item.node.cured,
-      death: acc.death + +item.node.death
+  const { total, cured, death } = data.reduce(
+    (acc, item) => {
+      return {
+        total: acc.total + +item.node.infected,
+        cured: acc.cured + +item.node.cured,
+        death: acc.death + +item.node.death,
+      }
+    },
+    {
+      total: 0,
+      cured: 0,
+      death: 0,
     }
-  }, {
-    total: 0, cured: 0, death: 0
-  })
+  )
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Nước</th>
-          <th>Số ca</th>
-          <th>Chữa khỏi</th>
-          <th>Tử vong</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map(({ node: item }) => (
-          <tr key={item.WB_A3}>
-            <td>{item.name}</td>
-            <td>{item.infected}</td>
-            <td>{item.cured}</td>
-            <td>{item.death}</td>
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Nước</th>
+            <th>Số ca</th>
+            <th>Chữa khỏi</th>
+            <th>Tử vong</th>
           </tr>
-        ))}
-        <tr>
-          <td>Tổng số ca</td>
-          <td>{total}</td>
-          <td>{cured}</td>
-          <td>{death}</td>
-        </tr>
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map(({ node: item }) => (
+            <tr key={item.WB_A3}>
+              <td>{item.name}</td>
+              <td>{item.infected}</td>
+              <td>{item.cured}</td>
+              <td>{item.death}</td>
+            </tr>
+          ))}
+          <tr>
+            <td>Tổng số ca</td>
+            <td>{total}</td>
+            <td>{cured}</td>
+            <td>{death}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div>Cập nhật 02/02/2020. Nguồn: WHO</div>
+    </>
   )
 }
 
