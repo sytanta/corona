e08d569a29a5e2d9f246870bc1096cc3a4505449
Tan Sy <sytanta@yahoo.com>
2020-02-04 15:50:45 +0700

    Update vn infection data

--- src/components/data-table-vn.js
+++ src/components/data-table-vn.js
@@ -44,7 +44,7 @@ const DataTableVN = ({ data }) => {
           </tr>
         </tbody>
       </table>
-      <div>Cập nhật: 09:20AM 04/02/2020</div>
+      <div>Cập nhật: 03:30PM 04/02/2020</div>
     </>
   )
 }
