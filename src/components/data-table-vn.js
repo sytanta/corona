3b3032404b3902df566147895cd4268daf3f7fbb
Tan Sy <sytanta@yahoo.com>
2020-02-21 08:18:46 +0700

    Update infection data

--- src/components/data-table-vn.js
+++ src/components/data-table-vn.js
@@ -44,7 +44,7 @@ const DataTableVN = ({ data }) => {
           </tr>
         </tbody>
       </table>
-      <div>Cập nhật: 04:00PM 20/02/2020</div>
+      <div>Cập nhật: 08:00AM 21/02/2020</div>
     </>
   )
 }
--- src/components/data-table-world.js
+++ src/components/data-table-world.js
@@ -39,7 +39,7 @@ const DataTableWorld = ({ data }) => {
           </tr>
         </tbody>
       </table>
-      <div>Cập nhật 19/02/2020. Nguồn: WHO</div>
+      <div>Cập nhật 20/02/2020. Nguồn: WHO</div>
     </>
   )
 }
