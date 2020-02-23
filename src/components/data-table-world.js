de755bb258aec73e7cc2428d8e5fd82dea87cfbe
Tan Sy <sytanta@yahoo.com>
2020-02-23 08:51:26 +0700

    Update infection data

--- src/components/data-table-world.js
+++ src/components/data-table-world.js
@@ -39,7 +39,7 @@ const DataTableWorld = ({ data }) => {
           </tr>
         </tbody>
       </table>
-      <div>Cập nhật 21/02/2020. Nguồn: WHO</div>
+      <div>Cập nhật 22/02/2020. Nguồn: WHO</div>
     </>
   )
 }
