cf25c968c6e8ce997cf594f85b0b5c6bf34e6c2b
Tan <sytanta@yahoo.com>
2020-04-03 08:44:19 +0700

    Update infection data

--- src/components/data-table-vn.js
+++ src/components/data-table-vn.js
@@ -56,7 +56,7 @@ const DataTableVN = ({ data }) => {
           </tr>
         </tbody>
       </table>
-      <div>Cập nhật: 8:00AM 02/04/2020</div>
+      <div>Cập nhật: 8:00AM 03/04/2020</div>
     </>
   )
 }
