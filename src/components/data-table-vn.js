88191e8b0e4360d3a98abc45d4bcc99bb4e971b1
Tan <sytanta@yahoo.com>
2020-04-23 12:44:21 +0700

    Update vn data

--- src/components/data-table-vn.js
+++ src/components/data-table-vn.js
@@ -56,7 +56,7 @@ const DataTableVN = ({ data }) => {
           </tr>
         </tbody>
       </table>
-      <div>Cập nhật: 8:00AM 22/04/2020</div>
+      <div>Cập nhật: 8:00AM 23/04/2020</div>
     </>
   )
 }
